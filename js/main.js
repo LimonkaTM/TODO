9a91fb7cdc101db7f3ba2bd8697431a259d53441
LimonkaTM <etuzin@mail.ru>
2021-11-13 17:20:32 +0300

    some changes

--- js/main.js
+++ js/main.js
@@ -1,55 +1,99 @@
 const $inputTodo = document.querySelector('.input-todo');
 const $btnTodoAdd = document.querySelector('.button-todo-add');
 const $todos = document.querySelector('.todos');
+const $main = document.querySelector('.main');
+const arr = localStorage.getItem('goals') === null ? [] : JSON.parse(localStorage.getItem('goals'));
 
-$btnTodoAdd.addEventListener('click', function() {
-  if ($inputTodo.value.length != 0) {
-    $todos.appendChild( createGoal($inputTodo.value) ) 
-    $inputTodo.value = "";
-  }
+const renderElementsFromLocalStorage = (arr) => {
+  arr.forEach( (item) => {
+    const $todo = document.createElement('div');
+    const $todoText = document.createElement('p');
+    const $buttonTodoDelete = document.createElement('button');
 
-  deleteGoal()
-})
+    $todo.classList.add('todo')
+    $todoText.classList.add('todo-text')
+    $buttonTodoDelete.classList.add('button')
+    $buttonTodoDelete.classList.add('button-todo-delete')
+    $buttonTodoDelete.name = 'todo-deletor';
+    $buttonTodoDelete.dataset.id = item.id;
+    
+    $todoText.innerText = item.goalText;
+    
+    $todo.appendChild($todoText)
+    $todo.appendChild($buttonTodoDelete)
 
-function createGoal(goalText) {
+    $todos.append($todo)
+  })
+
+  return $todos
+};
+
+renderElementsFromLocalStorage(arr);
+
+const createElement = (goalText) => {
   const $todo = document.createElement('div');
   const $todoText = document.createElement('p');
   const $buttonTodoDelete = document.createElement('button');
 
-  $todoText.innerText = goalText;
-  $buttonTodoDelete.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24">
-  <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-12v-2h12v2z"/>
-</svg>`;
+  const counter = arr.length; 
 
   $todo.classList.add('todo')
   $todoText.classList.add('todo-text')
   $buttonTodoDelete.classList.add('button')
   $buttonTodoDelete.classList.add('button-todo-delete')
-
+  $buttonTodoDelete.name = 'todo-deletor';
+  $buttonTodoDelete.dataset.id = counter;
+  
+  $todoText.innerText = goalText;
+  
   $todo.appendChild($todoText)
   $todo.appendChild($buttonTodoDelete)
+  
+  const toDoObj = {id: counter, goalText,};
+
+  arr.push(toDoObj)
+
+  if (!localStorage.getItem('goals')) {
+    localStorage.setItem('goals', JSON.stringify(arr))
+  } else {
+    localStorage.removeItem('goals')
+    localStorage.setItem('goals', JSON.stringify(arr))
+  }
 
   return $todo
-}
+};
 
-function checkKeyPrassed(event) {
+$btnTodoAdd.addEventListener('click', () => {
   if ($inputTodo.value.length != 0) {
-    if (event.keyCode == 13) {
-      $todos.appendChild( createGoal($inputTodo.value) ) 
-      $inputTodo.value = "";
+    $todos.appendChild( createElement($inputTodo.value) ) 
+    
+    $inputTodo.value = '';
+  }
+})
+
+$todos.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  const targetId = e.target.getAttribute('data-id');
+  let targetIndex
+
+  arr.forEach( (item, index) => {
+    if (item.id == targetId) {
+      targetIndex = index;
     }
-  }
+  })
+  arr.splice(targetIndex, 1)
   
-  deleteGoal()
-}
+  localStorage.removeItem('goals')
+  $todos.innerHTML = '';
+  localStorage.setItem('goals', JSON.stringify(arr))
+  
+  renderElementsFromLocalStorage(arr)
+})
 
-function deleteGoal() {
-  const arrBtnTodoDelete = document.querySelectorAll('.button-todo-delete');
-  const arrTodo = document.querySelectorAll('.todo');
-
-  for (let i = 0; i < arrBtnTodoDelete.length; i++){
-    arrBtnTodoDelete[i].addEventListener('click', function() {
-      arrTodo[i].remove()
-    })
+window.addEventListener('keydown', (e) => {
+  if (e.code == 'Enter' || e.key == 'Enter') {
+    $todos.appendChild( createElement($inputTodo.value) )
+    $inputTodo.value = '';
   }
-}
+})
